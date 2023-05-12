28a379d2ee66c9098e9a7538d29d901c507abb95
Janagan-Ra <47286607+Janagan-Ra@users.noreply.github.com>
2023-05-12 11:34:56 -0400

    Finished Filter
    Functional filter that doesn't save attachments to save space.

--- model.py
+++ model.py
@@ -1,29 +1,32 @@
-
+import numpy as np
 import pandas as pd
 from sklearn import svm
 from sklearn.feature_extraction. text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
+df = pd.read_csv('spam_ham_dataset.csv')
+message = df['text']
+# Spam = 1 o/w 0
+label = df['label_num']
+
+countVector = CountVectorizer()
+vMessage = countVector.fit_transform(message)
+#80:20 split
+message_train, message_test, label_train, label_test = train_test_split(vMessage,label,test_size=0.2)
+
+#Support vector Classification model was used with linear regression
+# When looking at the data we see a linear boundary line hence the choice
+model = svm.SVC(kernel='linear')
+model.fit(message_train,label_train)
+if __name__ == "__main__":
+    print(model.score(message_test,label_test))
+    # Average  97% +-1% 
+
 def makefilter():
-    df = pd.read_csv('spam_ham_dataset.csv')
-    message = df['text']
-    # Spam = 1 o/w 0
-    label = df['label_num']
-    countVector = CountVectorizer()
-    vMessage = countVector.fit_transform(message)
-    #80:20 split
-    message_train, message_test, label_train, label_test = train_test_split(vMessage,label,test_size=0.2)
-
-    #Support vector Classification model was used with linear regression
-    # When looking at the data we see a linear boundary line hence the choice
-    model = svm.SVC(kernel='linear')
-    model.fit(message_train,label_train)
-    if __name__ == "__main__":
-        print(model.score(message_test,label_test))
-        # Average  97% +-1% 
-
-    return model
+    return model,countVector
 
 if __name__ == "__main__":
-    makefilter()
+    text = np.array(df['text'][3]).reshape(1,)
+    model.predict(countVector.transform(text))
+    
