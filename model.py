1c8cee2dbf05043aa918e2cd181c6d304cce07d1
Janagan-Ra <47286607+Janagan-Ra@users.noreply.github.com>
2023-05-12 08:27:47 -0400

    model function
    made a model function to be used by filter

--- model.py
+++ model.py
@@ -5,19 +5,23 @@ from sklearn.feature_extraction. text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-df = pd.read_csv('spam_ham_dataset.csv')
-message = df['text']
-# Spam = 1 o/w 0
-label = df['label_num']
-countVector = CountVectorizer()
-vMessage = countVector.fit_transform(message)
-#80:20 split
-message_train, message_test, label_train, label_test = train_test_split(vMessage,label,test_size=0.2)
+def makefilter():
+    df = pd.read_csv('spam_ham_dataset.csv')
+    message = df['text']
+    # Spam = 1 o/w 0
+    label = df['label_num']
+    countVector = CountVectorizer()
+    vMessage = countVector.fit_transform(message)
+    #80:20 split
+    message_train, message_test, label_train, label_test = train_test_split(vMessage,label,test_size=0.2)
 
-#Support vector Classification model was used with linear re
-model = svm.SVC(kernel='linear')
-model.fit(message_train,label_train)
+    #Support vector Classification model was used with linear regression
+    # When looking at the data we see a linear boundary line hence the choice
+    model = svm.SVC(kernel='linear')
+    model.fit(message_train,label_train)
+
+    return model
 
 if __name__ == "__main__":
     print(model.score(message_test,label_test))
-    # Average 95% +-1% 
+    # Average  97% +-1% 
